199c784ca0ac321ef71b253490d8d2131fb0befd
nmfauzan <nadhifmmff@gmail.com>
2022-08-04 14:28:29 +0700

    Logic fixed

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ retur = []
 
 class PrintJSon(Resource):
     def get(self, a, b):
-        for x in range(a, b):
+        for x in range(a-1, b-1):
             Urla = "https://pse.kominfo.go.id/static/json-static/LOKAL_TERDAFTAR/"
             Urlb = ".json?page[page]=1&page[limit]=10&filter[search_term]="
             Url = Urla + str(x) + Urlb
             r = requests.get(Url)
             retur.append(r.json())
             print(retur)
-        dumpname = 'pselokal' + str(a+1) + '-' + str(b+1) + '.json'
+        dumpname = 'pselokal' + str(a) + '-' + str(b) + '.json'
         with open(dumpname, 'w') as json_file:
             json.dump(retur, json_file)
         return retur
